f88392ded3848e6054988206e2e24f92497ac9ed
chanuka1012 <chanukarathnayake1012@gmail.com>
2025-08-06 16:54:36 +0530

    Home

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,11 +3,17 @@ import './App.css'
 import React from 'react'
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import Hero from './Components/Hero';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-black text-white scroll-smooth">
        <Navbar/>
+       <div className="max-w-6xl mx-auto px-4">
+         <section id="home">
+          <Hero/>
+         </section>
+       </div>
        <Footer/>
     </div>
   );
