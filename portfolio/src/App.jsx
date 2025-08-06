28cdd6eb30d3459b2cd4ad19855c7b447890c4c2
chanuka1012 <chanukarathnayake1012@gmail.com>
2025-08-06 15:59:55 +0530

    Footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import React from 'react'
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div className="min-h-screen bg-black text-white scroll-smooth">
        <Navbar/>
+       <Footer/>
     </div>
   );
 
